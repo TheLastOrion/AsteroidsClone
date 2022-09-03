2840a129467487c84f60aa2d0fbb94aed1b2d32c
Aldoru Gürdilek <aldorug@yahoo.com>
2022-09-03 23:23:52 +0300

    More Cleanup
    Transform calculations are moved into Utils Class, as Asteroids are likely going to use same teleport. Avoiding code repetition.

--- Assets/Scripts/PlayerMoveControl.cs
+++ Assets/Scripts/PlayerMoveControl.cs
@@ -1,14 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMoveControl : MonoBehaviour
 {
     private Rigidbody m_rigidbody;
     private Collider m_collider;
-    [SerializeField][Range(5f, 15f)] private float m_accelerationFactor = 10f;
+    [SerializeField] [Range(5f, 15f)] private float m_accelerationFactor = 10f;
     [SerializeField] [Range(5f, 15f)] private float m_turnSpeed = 5f;
-    
     public void Start()
     {
         GameEvents.BorderExit += GameEventsOnBorderExit;
@@ -18,43 +16,16 @@ public class PlayerMoveControl : MonoBehaviour
 
     private void GameEventsOnBorderExit(BorderType borderType, Collider teleportCollider)
     {
-        float distanceBetweenTopAndBottom = Mathf.Abs(GameObject.Find("BottomBorder").transform.position.y - GameObject.Find("TopBorder").transform.position.y);
-        float distanceBetweenLeftAndRight = Mathf.Abs(GameObject.Find("LeftBorder").transform.position.x - GameObject.Find("RightBorder").transform.position.x);;
-        Debug.LogFormat("Distance Between Top & Bottom: {0}   Distance Between Left & Right: {1} ", distanceBetweenTopAndBottom, distanceBetweenLeftAndRight);
         if (teleportCollider == m_collider)
         {
-            switch (borderType)
-            {
-                case BorderType.Bottom:
-                    
-                    transform.position = new Vector3(transform.position.x, transform.position.y + distanceBetweenTopAndBottom, 0);
-                    break;
-                case BorderType.Left:
-                    transform.position = new Vector3(transform.position.x + distanceBetweenLeftAndRight, transform.position.y, 0);
-
-                    break;
-                case BorderType.Right:
-                    transform.position = new Vector3(transform.position.x -1f * + distanceBetweenLeftAndRight, transform.position.y , 0);
-
-                    break;
-                case BorderType.Top:
-                    transform.position = new Vector3(transform.position.x, transform.position.y + (-1f * distanceBetweenTopAndBottom), 0);
-
-                    break;
-                default:
-                    break;
-                
-            }
+            transform.position = CalculationUtils.FindTeleportPlace(transform, borderType);
         }
     }
 
     public void Update()
     {
-        
         if (Input.GetKey(KeyCode.W)) 
         {
-            // m_rigidbody.AddForce(Vector3.up * (transform.rotation.z * m_accelerationFactor) + 
-            //                      Vector3.right * (transform.rotation.z * m_accelerationFactor));
             m_rigidbody.AddRelativeForce(Vector3.up * m_accelerationFactor);
         }
 
---
+++ Assets/Scripts/Utility/CalculationUtils.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CalculationUtils
+{
+    public static Vector3 FindTeleportPlace(Transform transform, BorderType borderType)
+    {
+
+        
+            switch (borderType)
+            {
+                case BorderType.Bottom:
+
+                    transform.position = new Vector3(transform.position.x,
+                        transform.position.y + FindDistanceBetweenTopAndBottom(), 0);
+                    break;
+                case BorderType.Left:
+                    transform.position = new Vector3(transform.position.x + FindDistanceBetweenLeftAndRight(),
+                        transform.position.y, 0);
+
+                    break;
+                case BorderType.Right:
+                    transform.position = new Vector3(transform.position.x - 1f * +FindDistanceBetweenLeftAndRight(),
+                        transform.position.y, 0);
+
+                    break;
+                case BorderType.Top:
+                    transform.position = new Vector3(transform.position.x,
+                        transform.position.y + (-1f * FindDistanceBetweenTopAndBottom()), 0);
+
+                    break;
+                default:
+                    Debug.LogErrorFormat("No Border Found, Probably a bug.");
+                    break;
+
+            }
+
+            return transform.position;
+
+    }
+
+    public static float FindDistanceBetweenTopAndBottom()
+    {
+        return Mathf.Abs(GameObject.Find("BottomBorder").transform.position.y - GameObject.Find("TopBorder").transform.position.y);
+
+    }
+
+    public static float FindDistanceBetweenLeftAndRight()
+    {
+        return Mathf.Abs(GameObject.Find("LeftBorder").transform.position.x - GameObject.Find("RightBorder").transform.position.x);
+
+    }
+}
