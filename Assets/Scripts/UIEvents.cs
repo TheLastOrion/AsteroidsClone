6710cac39262e1c6a12cfce2ad12567c0260a6fd
Aldoru Gürdilek <aldorug@yahoo.com>
2022-09-03 15:17:20 +0300

    Skeleton Event Scripts added

---
+++ Assets/Scripts/UIEvents.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEvents : MonoBehaviour
+{
+    public event Action MoveForwardPressed;
+    public event Action MoveRightPressed;
+    public event Action MoveLeftPressed;
+    public event Action MoveBackPressed;
+}
