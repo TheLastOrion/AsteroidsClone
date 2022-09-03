e13e4b8e70988917716d03d044d1ffe54d2a99ad
Aldoru Gürdilek <aldorug@yahoo.com>
2022-09-03 15:32:33 +0300

    Test Scripts Folder & Collider Test Script
    Test Scripts Folder & Collider Test Script, also corrected some prefabs

---
+++ Assets/Scripts/TestScripts/ColliderTestScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ColliderTestScript : MonoBehaviour
+{
+    private BoxCollider m_boxCollider; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_boxCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.LogFormat("Trigger! Collider: {0} Collision: {1}", this.gameObject.name, other.gameObject.name);
+    }
+}
